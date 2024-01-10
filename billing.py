6fa7d4ad7877803cffca3b17dcabe6106e215838
allibek <olegpavlovg@gmail.com>
2024-01-10 14:52:45 +0700

    billing.py

--- billing.py
+++ billing.py
@@ -5,7 +5,7 @@ from rgwadmin import RGWAdmin
 
 access_key = '****************************************'
 secret_key = '****************************************'
-server = '********************************************'
+server =     '****************************************'
 log_path = '/var/log/billing.log'
 db_path = '/backup/billing.db'
 debug = True
@@ -13,82 +13,64 @@ debug = True
 timeout=600
 rgw = RGWAdmin(access_key=access_key, secret_key=secret_key, server=server, timeout=timeout)
 
-def get_stat():
-    result_DF = pd.DataFrame()
-    users = rgw.get_users()
-    for user in users:
+def init_db(path):
+    conn = sqlite3.connect(path)
+    c = conn.cursor()
+    c.execute('''CREATE TABLE IF NOT EXISTS billing (
+                    id INTEGER PRIMARY KEY AUTOINCREMENT, 
+                    timestamp DATETIME DEFAULT CURRENT_TIMESTAMP, 
+                    user_id TEXT NOT NULL, 
+                    tenant_id TEXT, 
+                    user_stats TEXT, 
+                    user_usage TEXT, 
+                    buckets TEXT, 
+                    buckets_usage TEXT )''')
+    conn.commit()
+    conn.close()
+
+def get_stat(path):
+    conn = sqlite3.connect(path)
+    c = conn.cursor()
+    all_users = rgw.get_users()
+    for user in all_users:
+        bucket_data = []
         user_stats = rgw.get_user(user, stats=True)
-        buckets = rgw.get_bucket(uid=user)
-        buckets_usage = rgw.get_usage(uid=user, show_entries=True)
-        for entry in buckets_usage['entries']:
-            for bucket in entry['buckets']:
-                if bucket['bucket'] in buckets:
-                    df = pd.DataFrame(index=[str(time.time())])
-                    df['user_id'] = str(user_stats['user_id'])
-                    df['tenant_id'] = str(user_stats['tenant'])
-                    df['stats_size_kb_actual'] = user_stats['stats']['size_kb_actual']
-                    df["stats_num_objects"] = user_stats['stats']['num_objects']
-                    if user_stats['tenant']:
-                        df['bucket'] = df['tenant_id'][0] + '/' + bucket['bucket']
-                    else:
-                        df['bucket'] = bucket['bucket']
-                    if debug:
-                        print('*******************************************************************************************************************************')
-                        print('user_stats: ', user_stats)
-                        print('user_id: ', df['user_id'][0])
-                        print('tenant_id: ', df['tenant_id'][0])
-                        print('bucket: ', df['bucket'][0])
-                        print('stats_size_kb_actual: ', df['stats_size_kb_actual'][0])
-                        print('stats_num_objects: ', df['stats_num_objects'][0])
-                    for category in bucket['categories']:
-                        category_name = category['category']
-                        if category_name:
-                            if category['bytes_sent']:
-                                df[category_name + '_bs'] = category['bytes_sent']
-                                if debug:
-                                    print(category_name + '_bs: ', df[category_name + '_bs'][0])
-                            if category['bytes_received']:
-                                df[category_name + '_br'] = category['bytes_received']
-                                if debug:
-                                    print(category_name + '_br: ', df[category_name + '_br'][0])
-                            if category['ops']:
-                                df[category_name + '_ops'] = category['ops']
-                                if debug:
-                                    print(category_name + '_ops: ', df[category_name + '_ops'][0])
-                            if category['successful_ops']:
-                                df[category_name + '_sops'] = category['successful_ops']
-                                if debug:
-                                    print(category_name + '_sops: ', df[category_name + '_sops'][0])
-                    bucket_usage = rgw.get_bucket(bucket=df['bucket'][0])
-                    if debug:
-                        print('bucket_usage: ', bucket_usage['usage'])
-                    if bucket_usage['placement_rule']:
-                        df['placement_rule'] = bucket_usage['placement_rule']
-                    if bucket_usage['usage']:
-                        if bucket_usage['usage']['rgw.main']['size_kb_actual']:
-                            df['bucket_size_kb_actual'] = bucket_usage['usage']['rgw.main']['size_kb_actual']
-                        if bucket_usage['usage']['rgw.main']['num_objects']:
-                            df['bucket_num_objects'] = bucket_usage['usage']['rgw.main']['num_objects']
-                    else:
-                        df['bucket_size_kb_actual'] = 0
-                        df['bucket_num_objects'] = 0
-                    if debug:
-                        print('placement_rule: ', df['placement_rule'][0])
-                        print('bucket_size_kb_actual: ', df['bucket_size_kb_actual'][0])
-                        print('bucket_num_objects: ', df['bucket_num_objects'][0])
-                        print('*******************************************************************************************************************************')
-                    result_DF = pd.concat([result_DF, df])
-    conn = sqlite3.connect(db_path)
-    try:
-        result_DF.to_sql(name='billing', con=conn, if_exists='append', index=True)
-    except Exception as e:
-        result_DF = pd.concat([result_DF, pd.read_sql('SELECT * FROM billing limit 1', con=conn)])
-        result_DF.to_sql(name='billing', con=conn, if_exists = 'replace', index=True)
+        user_stats_str = ''
+        if user_stats['stats']:
+            user_stats_str = json.dumps(user_stats['stats'])
+        user_id = ''
+        if user_stats['user_id']:
+            user_id = user_stats['user_id']
+        tenant_id = ''
+        if user_stats['tenant']:
+            tenant_id = user_stats['tenant']
+        user_usage = json.dumps(rgw.get_usage(uid=user, show_entries=True, show_summary=True))
+        buckets=rgw.get_bucket(uid=user)
+        buckets_str = json.dumps(buckets)
+        for bucket in buckets:
+            bucket_info = {}
+            if tenant_id:
+                bucket = tenant_id + '/' + bucket
+            bucket_info['bucket'] = bucket
+            rgw_get_bucket = rgw.get_bucket(bucket=bucket)
+            bucket_info['placement_rule'] = ''
+            if rgw_get_bucket['placement_rule']:
+                bucket_info['placement_rule'] = rgw_get_bucket['placement_rule']
+            bucket_info['usage'] = ''
+            if rgw_get_bucket['usage']:
+                bucket_info['usage'] = rgw_get_bucket['usage']
+            bucket_data.append(bucket_info)
+        buckets_usage = json.dumps({'buckets': bucket_data})
+
+        c.execute("INSERT INTO billing (user_id,tenant_id,user_stats,user_usage,buckets,buckets_usage) VALUES (?, ?, ?, ?, ?, ?)",
+              (user_id,tenant_id,user_stats_str,user_usage,buckets_str,buckets_usage))
+    conn.commit()
     conn.close()
 
 if __name__ == '__main__':
     try:
-        get_stat()
+        init_db(db_path)
+        get_stat(db_path)
     except Exception as e:
         if debug:
            print(e)
